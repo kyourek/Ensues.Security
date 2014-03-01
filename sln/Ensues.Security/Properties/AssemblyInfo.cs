2824828ffe1ed1a2007ea1529a1e4c8a3ce34a33
Ken Yourek <ken@yourek.net>
2014-02-28 18:33:09 -0600

    Added NuGet and npcknpsh files.

--- sln/Ensues.Security/Properties/AssemblyInfo.cs
+++ sln/Ensues.Security/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Ensues.Security")]
-[assembly: AssemblyDescription("Ensues Cryptography")]
+[assembly: AssemblyDescription("Ensues Security")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ken Yourek")]
 [assembly: AssemblyProduct("Ensues.Security")]
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("8b9ce40c-a3c7-45a1-88b0-417b09b30ac2")]
 
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyVersion("0.1.1")]
+[assembly: AssemblyFileVersion("0.1.1.3")]
+[assembly: AssemblyInformationalVersion("0.1.1")]
 
 [assembly: InternalsVisibleTo("Ensues.Security.Tests")]
